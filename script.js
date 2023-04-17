c1323c85766bf32aeddc2a9304210fa4e9bea99e
Dragomy <dragomy1.02@gmail.com>
2023-04-17 20:23:25 +0200

    Multiple browser project started

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const searchButton = document.querySelector('.search button');
 const googleResults = document.querySelector('.google');
 const bingResults = document.querySelector('.bing');
 const youtubeResults = document.querySelector('.youtube');
+const resultsContainer = document.querySelector('.results');
 
 searchButton.addEventListener('click', search);
 searchInput.addEventListener('keyup', function(event) {
@@ -22,5 +23,35 @@ function search() {
 		googleResults.innerHTML = '<iframe src="' + googleUrl + '"></iframe>';
 		bingResults.innerHTML = '<iframe src="' + bingUrl + '"></iframe>';
 		youtubeResults.innerHTML = '<iframe src="' + youtubeUrl + '"></iframe>';
+
+		// Add click event listener to each result container
+		const resultContainers = document.querySelectorAll('.results div');
+		resultContainers.forEach(container => {
+			container.addEventListener('click', function(event) {
+				event.stopPropagation(); // Prevent event from bubbling up to the results container
+				// Remove active class from other containers
+				resultContainers.forEach(c => c.classList.remove('active'));
+				// Toggle active class on clicked container
+				this.classList.toggle('active');
+			});
+		});
 	}
 }
+
+// Add click event listener to results container to remove active class on click outside of containers
+resultsContainer.addEventListener('click', function(event) {
+	if (event.target === resultsContainer) {
+		const activeContainer = document.querySelector('.results div.active');
+		if (activeContainer) {
+			activeContainer.classList.remove('active');
+		}
+	}
+});
+
+// Add click event listener to document to remove active class on click outside of results container
+document.addEventListener('click', function(event) {
+	const activeContainer = document.querySelector('.results div.active');
+	if (activeContainer && !activeContainer.contains(event.target)) {
+		activeContainer.classList.remove('active');
+	}
+});
