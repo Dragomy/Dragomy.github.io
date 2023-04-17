476a4f421a339d04443371efec140597ef770823
Dragomy <dragomy1.02@gmail.com>
2023-04-17 20:05:19 +0200

    trying out something

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const searchInput = document.querySelector('.search input');
+const searchButton = document.querySelector('.search button');
+const googleResults = document.querySelector('.google');
+const bingResults = document.querySelector('.bing');
+const youtubeResults = document.querySelector('.youtube');
+
+searchButton.addEventListener('click', search);
+searchInput.addEventListener('keyup', function(event) {
+	if (event.key === 'Enter') {
+		search();
+	}
+});
+
+function search() {
+	const query = searchInput.value;
+
+	if (query.trim() !== '') {
+		const googleUrl = 'https://www.google.com/search?q=' + query;
+		const bingUrl = 'https://www.bing.com/search?q=' + query;
+		const youtubeUrl = 'https://www.youtube.com/results?search_query=' + query;
+
+		googleResults.innerHTML = '<iframe src="' + googleUrl + '"></iframe>';
+		bingResults.innerHTML = '<iframe src="' + bingUrl + '"></iframe>';
+		youtubeResults.innerHTML = '<iframe src="' + youtubeUrl + '"></iframe>';
+	}
+}
