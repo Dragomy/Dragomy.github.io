59732f6a2887d9aea6e31c7848e0c2f1912f38c2
Dragomy <dragomy1.02@gmail.com>
2023-06-22 16:50:38 +0200

    Does it work?

--- scriptMobile.js
+++ scriptMobile.js
@@ -1,16 +1,14 @@
+//Darken Purple when scroling down
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
     var body = document.querySelector('body');
     var maxScroll = document.documentElement.scrollHeight - window.innerHeight;
     var opacity = scrollPosition / maxScroll;
-  
-    // Calculate the darkened purple color
     var darkenedPurple = darkenColor('#4a148c', opacity);
-  
+
     body.style.backgroundColor = darkenedPurple;
   });
   
-  // Function to darken a given color by a specified opacity
   function darkenColor(color, opacity) {
     var hex = color.replace('#', '');
     var r = parseInt(hex.substring(0, 2), 16);
