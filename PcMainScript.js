9e19228b93a4cedfdd41485cc4903c4e0b0e03c0
Dragomy <dragomy1.02@gmail.com>
2023-10-10 01:32:41 +0200

    Styling

--- PcMainScript.js
+++ PcMainScript.js
@@ -1,18 +1,18 @@
 //Function used to toggle the Curriculum Vitae Window
 function toggleDisplay(elementId) {
-    const element = document.getElementById(elementId);
-    element.style.display = (element.style.display === 'none') ? 'block' : 'none';
-  }
-  
-  function addButtonClickListener(buttonId, divId) {
-    const button = document.getElementById(buttonId);
-    const div = document.getElementById(divId);
-    button.addEventListener('click', function() {
-      toggleDisplay(divId);
-    });
-  }
-  
-  addButtonClickListener('CurViButton', 'CurriculumVitae');
-  addButtonClickListener('CurViClose', 'CurriculumVitae');
- 
-  
+  const element = document.getElementById(elementId);
+  element.style.display = (element.style.display === 'none') ? 'block' : 'none';
+}
+
+function addButtonClickListener(buttonId, divId) {
+  const button = document.getElementById(buttonId);
+  const div = document.getElementById(divId);
+  button.addEventListener('click', function () {
+    toggleDisplay(divId);
+  });
+}
+
+addButtonClickListener('CurViButton', 'CurriculumVitae');
+addButtonClickListener('CurViClose', 'CurriculumVitae');
+
+
