cabc16add74192df5363006fd1f4440e955f5c94
Dragomy <95567734+Dragomy@users.noreply.github.com>
2024-01-05 21:31:46 +0100

    Upgraded the pc script

--- PcMainScript.js
+++ PcMainScript.js
@@ -1,18 +1,1 @@
-//Function used to toggle the Curriculum Vitae Window
-function toggleDisplay(elementId) {
-  const element = document.getElementById(elementId);
-  element.style.display = (element.style.display === 'none') ? 'block' : 'none';
-}
-
-function addButtonClickListener(buttonId, divId) {
-  const button = document.getElementById(buttonId);
-  const div = document.getElementById(divId);
-  button.addEventListener('click', function () {
-    toggleDisplay(divId);
-  });
-}
-
-addButtonClickListener('CurViButton', 'CurriculumVitae');
-addButtonClickListener('CurViClose', 'CurriculumVitae');
-
-
+// Haha no JavaScript
